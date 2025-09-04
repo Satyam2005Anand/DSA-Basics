78846280bf7bb88e29aa6d5f360e1c006eb3325e
Satyam2005Anand <122661960+Satyam2005Anand@users.noreply.github.com>
2025-09-04 17:22:25 +0530

    Update prime factorisation.cpp

--- functions/basic/prime factorisation.cpp
+++ functions/basic/prime factorisation.cpp
@@ -1,1 +1,24 @@
+// Prime factorisation of a number
+#include<iostream>
+using namespace std;
 
+void primeFactorisation(int a){
+    while (a % 2 == 0){
+        cout << 2 << " ";
+        a /= 2;
+    }
+
+    for (int i = 3; i * i <= a; i+=2){
+        while (a % i == 0){
+            cout << i<< " ";
+            a /= i;
+        }
+    }
+    if (a > 2) cout << a;
+}
+int main(){
+    int n;
+    cin >> n;
+    primeFactorisation(n);
+    return 0;
+}
